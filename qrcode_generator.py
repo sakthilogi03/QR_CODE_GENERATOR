b60da3c339ed3dbdad0a1187ee382dc036af3aa7
sakthilogi03 <85428330+sakthilogi03@users.noreply.github.com>
2021-08-11 00:35:57 +0530

    Update qrcode_generator.py

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -1,3 +1,4 @@
+# First Import the pyqrcode module and also png module
 # Import QRCode from pyqrcode
 import pyqrcode
 import png
@@ -10,8 +11,8 @@ s = "linkedin.com/in/jayasakthi-j-r-45aa4a205"
 # Generate QR code
 url = pyqrcode.create(s)
   
-# Create and save the svg file naming "myqr.svg"
+# Create and save the svg file naming "myqrforlinkedin.svg"
 url.svg("myqrforlinkedin.svg", scale = 8)
   
-# Create and save the png file naming "myqr.png"
+# Create and save the png file naming "myqrforlinkedin.png"
 url.png('myqrforlinkedin.png', scale = 6)
